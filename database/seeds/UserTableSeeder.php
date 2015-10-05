edf90a567d8ccf1dfc5cee140ba09384486aa413
Ettienne Pitts <ettpitts@gmail.com>
2015-10-05 09:23:30 +0200

    User login started

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => env('ROOT_USERNAME'),
+            'email' => env('ROOT_EMAIL'),
+            'password' => bcrypt(env('ROOT_PASSWORD'))
+        ]);
+    }
+}
