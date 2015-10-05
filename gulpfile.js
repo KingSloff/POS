3cdd9d845de206be8a03d291ba8e4bc46b714ec4
Ettienne Pitts <ettpitts@gmail.com>
2015-10-05 08:29:23 +0200

    Initial style files done

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,19 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+
+    mix.styles([
+        'bootstrap.css',
+        'bootstrap-theme.css'
+    ]);
+
+    mix.scripts([
+        'bootstrap.js'
+    ]);
+
+    mix.version([
+        'css/all.css',
+        'js/all.js'
+    ]);
+
 });
