3b2fd29e3af20bfedde10dd88a82a7984f2e8985
Ettienne Pitts <ettpitts@gmail.com>
2016-01-07 11:59:15 +0200

    Another column added

--- resources/views/products/parts/product.blade.php
+++ resources/views/products/parts/product.blade.php
@@ -4,5 +4,6 @@
     <td><a href="{{route('product.show', $product)}}">{{$product->id}}</a></td>
     <td>{{$product->name}}</td>
     <td>{{$services->displayAmount($product->in_stock)}}</td>
+    <td>{{$services->displayCurrency($product->price)}}</td>
     <td>{{$services->displayCurrency($product->profit)}}</td>
 </tr>
--- resources/views/products/parts/products.blade.php
+++ resources/views/products/parts/products.blade.php
@@ -2,7 +2,8 @@
     <table class="table table-striped">
         <colgroup>
             <col span="1" style="width: 10%;">
-            <col span="1" style="width: 70%;">
+            <col span="1" style="width: 60%;">
+            <col span="1" style="width: 10%;">
             <col span="1" style="width: 10%;">
             <col span="1" style="width: 10%;">
         </colgroup>
@@ -11,6 +12,7 @@
             <td><strong>{!! \App\Traits\SortableTrait::link_to_sorting_action('id', 'Product') !!}</strong></td>
             <td><strong>{!! \App\Traits\SortableTrait::link_to_sorting_action('name') !!}</strong></td>
             <td><strong>{!! \App\Traits\SortableTrait::link_to_sorting_action('in_stock', 'Stock') !!}</strong></td>
+            <td><strong>{!! \App\Traits\SortableTrait::link_to_sorting_action('price') !!}</strong></td>
             <td><strong>{!! \App\Traits\SortableTrait::link_to_sorting_action('profit', 'Profit', true) !!}</strong></td>
         </tr>
         </thead>
